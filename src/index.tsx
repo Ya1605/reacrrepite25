506c19ae31a5e1e91801361946aa2908619354ae
YuliiaT <115080417+Za1605@users.noreply.github.com>
2025-04-06 21:40:20 +0300

    add layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider } from "react-router-dom-v5-compat";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routerConfig = createBrowserRouter(
+  [
+    {path:'/', element : 'Hello'}
+  ]
+)
+
+
+root.render(
+  
+  <RouterProvider router={routerConfig}></RouterProvider>
+
+  
+);
+function createBrowserRouter(arg0: { path: string; }[]) {
+  throw new Error('Function not implemented.');
+}
+
